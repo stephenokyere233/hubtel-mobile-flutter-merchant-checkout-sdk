9f3d68922d1d75c761e32e4048ad1bea440f99e6
albanybuipe96 <mohammedarahman96@gmail.com>
2023-11-16 17:21:08 +0000

    fixing exports

--- lib/hubtel_merchant_checkout_sdk.dart
+++ lib/hubtel_merchant_checkout_sdk.dart
@@ -1,5 +1,5 @@
 library unified_checkout_sdk;
 
-export '/src/core_ui/core_ui.dart' show ThemeConfig, InputField, AppPage, CustomButton, HubtelColors, HubtelButtonStyle;
+export '/src/core_ui/core_ui.dart' show ThemeConfig;
 export '/src/platform/models/models.dart' show PurchaseInfo, HubtelCheckoutConfiguration, PaymentStatus;
 export '/src/ux/home/checkout_screen.dart';
