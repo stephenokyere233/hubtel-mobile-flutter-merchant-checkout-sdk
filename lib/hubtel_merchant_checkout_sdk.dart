5dfab63c72f1242cf71f4dd4338d09049a521943
MarkAmoah <markamoah97@gmail.com>
2023-12-04 16:12:15 +0000

    expose themeConfig

--- lib/hubtel_merchant_checkout_sdk.dart
+++ lib/hubtel_merchant_checkout_sdk.dart
@@ -1,5 +1,6 @@
 library unified_checkout_sdk;
 
 
+export 'src/core_ui/text_style.dart' show ThemeConfig;
 export '/src/platform/models/models.dart' show PurchaseInfo, HubtelCheckoutConfiguration, UnifiedCheckoutPaymentStatus, CheckoutCompletionStatus;
 export '/src/ux/home/checkout_screen.dart';
